628da958a3653f38c33cb5870f91c79b44ddda66
Conor McCue <conormccue@gmail.com>
2019-11-08 20:32:47 -0800

    basic server

---
+++ lab6/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Dependencies
+
+//Require triggers the dotenv mode nodule to export to this file in the form of an object. the config method then establishes the user environment by parsing variables from the .env file.
+
+require('dotenv').config()
+
+//Simililary to the example above this next line exports the express module to this file and stores it in a variable express.
+const express = require('express');
+
+//cors is a node module to prevent cross-origin scripting
+const cors = require('cors');
+
+//Define app
+const app = express();
+app.use(cors);
+const PORT = process.env.PORT || 3000;
+
+//Routes
+console.log(app.get);
+app.get('/', (req, res) => {
+  res.status(200).send('You made it!');
+});
+
+//Start listening, think about this like an event listener(the whole server code) attached to the port
+//A node http.Server is returned
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
