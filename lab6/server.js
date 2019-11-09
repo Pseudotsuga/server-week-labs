6644aa24bf6b0ad213aa997420842b34106e132c
Conor McCue <conormccue@gmail.com>
2019-11-08 22:25:29 -0800

    resolve merge conflicts

--- lab6/server.js
+++ lab6/server.js
@@ -4,7 +4,7 @@
 
 //Require triggers the dotenv mode nodule to export to this file in the form of an object. the config method then establishes the user environment by parsing variables from the .env file.
 
-require('dotenv').config()
+require('dotenv').config();
 
 //Simililary to the example above this next line exports the express module to this file and stores it in a variable express.
 const express = require('express');
@@ -14,14 +14,16 @@ const cors = require('cors');
 
 //Define app
 const app = express();
-app.use(cors);
-const PORT = process.env.PORT || 3000;
+
+//Cors must be INVOKED
+app.use(cors());
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).send('You made it!');
-});
+app.get('/', (request,response) => response.send('You made it!'));
+
+app.get('/test', (req, res) => res.send('testing testing 123'));
 
 //Start listening, think about this like an event listener(the whole server code) attached to the port
 //A node http.Server is returned
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
