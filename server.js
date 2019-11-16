8e2c1f00c2023cb36d364bb07b959a7353e0b6b6
Conor McCue <conormccue@gmail.com>
2019-11-16 13:21:16 -0800

    complete lab6 card3

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 'use strict';
 
 //Dependencies
-
 //Require triggers the dotenv mode nodule to export to this file in the form of an object. the config method then establishes the user environment by parsing variables from the .env file.
-
 require('dotenv').config();
 
 //Simililary to the example above this next line exports the express module to this file and stores it in a variable express.
@@ -12,24 +10,25 @@ const express = require('express');
 //cors is a node module to prevent cross-origin scripting
 const cors = require('cors');
 
-//Define app
-const app = express();
 
+
+//Setup app
+const app = express();
 //Cors must be INVOKED
 app.use(cors());
 
+
 //Routes
 app.get('/', (request,response) => response.send('You made it!'));
-
-app.get('/test', (req, res) => res.send('testing testing 123'));
-
 app.get('/location', locationRouter);
+app.get('/weather', weatherRouter)
 
+// Helper Functions
 function locationRouter(request, response){
   const city = request.query.data;
   const geoData = require('./data/geo.json');
   const locationData = new Location(city, geoData);
-  response.send(locationData);
+  response.status(200).send(locationData);
 }
 
 function Location(city, geoData){
@@ -39,6 +38,23 @@ function Location(city, geoData){
   this.formatted_query = cityData.formatted_address;
   this.latitude = cityData.geometry.location.lat;
   this.longitude = cityData.geometry.location.lng;
+}
+
+function weatherRouter(req, res){
+  const darkSkyData = require('./data/darksky.json');
+  const forecast = new Weather(darkSkyData);
+  res.status(200).send(forecast);
+}
+
+function Weather(weatherData){
+  let weatherArr = weatherData.daily.data.map( dailyforecast => {
+    let newTime = new Date(dailyforecast.time * 1000).toDateString();
+    return dailyforecast = {
+      forecast: dailyforecast.summary,
+      time: newTime
+    }
+  });
+  return weatherArr;
 }
 
 //Start listening, think about this like an event listener(the whole server code) attached to the port
